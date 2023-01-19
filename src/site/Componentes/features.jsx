61780a25db50ef4b772cf13c8b334b4feac7f064
Andre Fernando Lara <andrefernandolara@hotmail.com>
2023-01-18 22:34:15 -0300

    Adicionando carrocel

--- src/site/Componentes/features.jsx
+++ src/site/Componentes/features.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 function Features(){
   return <section id="features">
-    <div class="container text-center">
-      <div id="features-box" class="row align-items-start">
-        <div class="col">
-          <img src="https://grafocapital.com.br/wp-content/uploads/2022/02/fixa-thumb.jpg" alt="" />
-          <h2>Renda fixa</h2>
+    <div class="container text-center ">
+      <div id="features-box" class="row align-items-start ">
+          <div class="col">
+            <img class="rounded" src="https://grafocapital.com.br/wp-content/uploads/2022/02/fixa-thumb.jpg" alt="" />
+            <h2>Renda fixa</h2>
         </div>
         <div class="col">
-          <img src="https://grafocapital.com.br/wp-content/uploads/2022/02/investimento-thumb.jpg" alt="" />
+          <img class="rounded" src="https://grafocapital.com.br/wp-content/uploads/2022/02/investimento-thumb.jpg" alt="" />
           <h2>Fundos de investimento</h2>
         </div>
         <div class="col">
-          <img src="https://grafocapital.com.br/wp-content/uploads/2022/02/fundos-thumb.jpg" alt="" />
+          <img class="rounded" src="https://grafocapital.com.br/wp-content/uploads/2022/02/fundos-thumb.jpg" alt="" />
           <h2>Fundos Imobiliários</h2>
-        </div>
+        </div> 
         <div class="col">
-          <img src="https://grafocapital.com.br/wp-content/uploads/2022/02/acoes.jpg" alt="" />
+          <img class="rounded" src="https://grafocapital.com.br/wp-content/uploads/2022/02/acoes.jpg" alt="" />
           <h2>Ações</h2>
         </div>
         <div class="col">
-          <img src="https://grafocapital.com.br/wp-content/uploads/2022/02/esg-thumb.jpg" alt="" />
+          <img  class="rounded" src="https://grafocapital.com.br/wp-content/uploads/2022/02/esg-thumb.jpg" alt="" />
           <h2>ESG</h2>
         </div>
         
